2db62afd3876affa347e9b0eba12a5cb170d7e29
namo-patil <namratapatil74738@gmail.com>
2022-05-18 00:41:43 +0530

    Added: JUnit

---
+++ src/main/java/com/bridgelabz/emailaddressvalidation/MultipleEmailAddressValidator.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.emailaddressvalidation;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MultipleEmailAddressValidator {
+    private static final String EMAIL_PATTERN = "^[a-z]+[_+-.]?[0-9a-z]+?[@][0-9a-z]+[.][a-z]{2,}([.][a-z]+)?[,]?";
+
+    public boolean validateEmail(String eMail) {
+        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+        Matcher matcher = pattern.matcher(eMail);
+        return matcher.matches();
+    }
+}
